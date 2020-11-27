0a3b40f7fee84f6f3a9184124248a9553c11559d
David Oster aka George Pasparakis <paspa@hotmail.com>
2020-11-27 21:09:37 +0200

    All sorting algorithms implemented
    - bucket sort
    - Timer

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -6,10 +6,10 @@
 package sort;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Vector;
 import models.TShirt;
+import models.enums.Size;
+import models.random.RandomTShirt;
 
 /**
  *
@@ -34,7 +34,7 @@ public class Sort {
         }
         return (arr2);
     }
-    
+
     private List<TShirt> bubbleSortTShirtsBySize(List<TShirt> arr, int sortingType) // sortingType = 0 - ASC
     // sortingType = 1 - DESC
     {
@@ -297,30 +297,64 @@ public class Sort {
             }
         }
     }
-    
-    
+
     // Step 1: divide to n + 1 buckets as the number of elements, 
-        // e.g. if we have 10 elements then we create 10 buckets
-        
-        // Step 2: place similar data to each bucket, 
-        // e.g. 1: { 10, 11, 22, 2, 19, 3, 17}
-        // n = 7, b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]
-        // b[1] = { 10, 11, 19, 17}
-        // b[2] = { 22, 2 }
-        // b[3] = { 3 }
-        
-        // e.g. 2: { 0, 0, 1, 3, 0, 4, 2, 3 }
-        // int n = randomTShirts.size();
-        // List<TShirt>[] buckets = new List<TShirt>[randomTShirts.size()]();
-        // b[0] = { 0, 0, 0 }
-        // b[1] = { 1, 1, 1, 1, 1 } <----- randomTShirts.get(22).getSize().ordinal() = 1
-        // b[2] = { 2 }
-        // b[3] = { 3, 3 }
-        // b[4] = { 4 }
-        
-        // Step 3: sort each bucket ??????????
-    public void bucketSortTShirts(List<TShirt> arr, int n) 
-    { 
-        
+    // e.g. if we have 10 elements then we create 10 buckets
+    // Step 2: place similar data to each bucket, 
+    // e.g. 1: { 10, 11, 22, 2, 19, 3, 17}
+    // n = 7, b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]
+    // b[1] = { 10, 11, 19, 17}
+    // b[2] = { 22, 2 }
+    // b[3] = { 3 }
+    // e.g. 2: { 0, 0, 1, 3, 0, 4, 2, 3 }
+    // int n = randomTShirts.size();
+    // List<TShirt>[] buckets = new List<TShirt>[randomTShirts.size()]();
+    // b[0] = { 0, 0, 0 }
+    // b[1] = { 1, 1, 1, 1, 1 } <----- randomTShirts.get(22).getSize().ordinal() = 1
+    // b[2] = { 2 }
+    // b[3] = { 3, 3 }
+    // b[4] = { 4 }
+    // Step 3: sort each bucket ??????????
+    public void bucketSortTShirts(List<TShirt> randomTShirts, int sortByAttribute, int sortingType) {
+        // Step 1 - Create buckets, Size.length
+        int noOfBuckets = Size.values().length;
+
+        // Step 1, create noOfBuckets buckets  
+        List<TShirt>[] buckets = new ArrayList[noOfBuckets];
+        for (int i = 0; i < noOfBuckets; i++) { // noOfBuckets
+            buckets[i] = new ArrayList<TShirt>(); // initialize the buckets
+        }
+
+        // Step 2, divide into buckets
+        for (TShirt tShirt : randomTShirts) {
+//              System.out.println(tShirt.getSize().ordinal()); // 3
+            switch (sortByAttribute) {
+                case 0:
+                    buckets[tShirt.getSize().ordinal()].add(tShirt);
+                    break;
+                case 1:
+                    buckets[tShirt.getColor().ordinal()].add(tShirt);
+                    break;
+                case 2:
+                    buckets[tShirt.getFabric().ordinal()].add(tShirt);
+                    break;
+            }
+        }
+
+        // Step 3, print
+        // just print the contents of each bucket
+        if (sortingType == 0) {
+//            for (int i = 0; i < noOfBuckets; i++) { // noOfBuckets
+//                for (int j = 0; j < buckets[i].size(); j++) {
+//                    System.out.println(buckets[i].get(j));
+//                }
+//            }
+        } else {
+//            for (int i = noOfBuckets - 1; i >= 0; i--) { // noOfBuckets
+//                for (int j = 0; j < buckets[i].size(); j++) {
+//                    System.out.println(buckets[i].get(j));
+//                }
+//            }
+        }
     }
 }
--- src/sortingtshirts/SortingTShirts.java
+++ src/sortingtshirts/SortingTShirts.java
@@ -7,8 +7,8 @@ package sortingtshirts;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 import models.TShirt;
+import models.Timer;
 import printing.Print;
 import models.random.RandomTShirt;
 import sort.Sort;
@@ -24,103 +24,37 @@ public class SortingTShirts {
     public static void main(String[] args) {
         Print p = new Print();
         Sort s = new Sort();
-        
-//        List<Integer> integers = new ArrayList<>();
-//        integers = Arrays.asList(new Integer[] {5,2,88,1,29,6,13});
-// 
-//        p.printArray(integers);
-//        p.printArray(s.bubbleSort(integers));
-        
-//        TShirt randomTShirt = new RandomTShirt();
-//        System.out.println(randomTShirt);
-        
-        int numberOfRandomTShirts = 5;
+        int numberOfRandomTShirts = 4000;
         System.out.println("Original Array");
         List<TShirt> randomTShirts = new ArrayList<>(numberOfRandomTShirts);
         for (int i = 0; i < numberOfRandomTShirts; i++) {
             randomTShirts.add(new RandomTShirt());
-            System.out.println(randomTShirts.get(i));
+//            System.out.println(randomTShirts.get(i));
         }
         
-//        System.out.println("\nSorted Array By Size Ascending");
-//        List<TShirt> bubbleSortRandomTShirtsBySize = s.bubbleSortTShirtsBySize(randomTShirts,0);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(bubbleSortRandomTShirtsBySize.get(i));
-//        }
-//        
-//        System.out.println("\nSorted Array By Size Descending");
-//        List<TShirt> bubbleSortRandomTShirtsBySize1 = s.bubbleSortTShirtsBySize(randomTShirts,1);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(bubbleSortRandomTShirtsBySize1.get(i));
-//        }
+        System.out.println("\nBucket Sorted Array By Size Ascending");
+        Timer bucketTimer = new Timer();
+        s.bucketSortTShirts(randomTShirts, 0, 0);
+        bucketTimer.stop();
+        System.out.println("Bucket Sort Time: " + bucketTimer.elapsedTime());
         
-//        System.out.println("\nSorted Array By Color Ascending");
-//        List<TShirt> bubbleSortRandomTShirtsBySize2 = s.bubbleSortTShirtsByColor(randomTShirts,0);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(bubbleSortRandomTShirtsBySize2.get(i));
-//        }
-//        
-//        System.out.println("\nSorted Array By Color Descending");
-//        List<TShirt> bubbleSortRandomTShirtsBySize3 = s.bubbleSortTShirtsByColor(randomTShirts,1);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(bubbleSortRandomTShirtsBySize3.get(i));
-//        }
-        
-//        System.out.println("\nSorted Array By Fabric Ascending");
-//        List<TShirt> bubbleSortRandomTShirtsBySize4 = s.bubbleSortTShirts(randomTShirts, 0, 2);
+        System.out.println("\nBubble Sorted Array By Size Ascending");
+        Timer bubbleTimer = new Timer();
+        List<TShirt> bubbleSortRandomTShirtsBySize4 = s.bubbleSortTShirts(randomTShirts, 0, 0);
+        bubbleTimer.stop();
 //        for (int i = 0; i < numberOfRandomTShirts; i++) {
 //            System.out.println(bubbleSortRandomTShirtsBySize4.get(i));
 //        }
-//        
-//        System.out.println("\nSorted Array By Fabric Descending");
-//        List<TShirt> bubbleSortRandomTShirtsBySize5 = s.bubbleSortTShirts(randomTShirts, 1, 2);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(bubbleSortRandomTShirtsBySize5.get(i));
-//        }
-        
-//        System.out.println("\nQuick Sorted Array By Size Ascending");
-//        List<TShirt> quickSortRandomTShirtsBySize = new ArrayList<TShirt>(randomTShirts);
-//        s.quickSortTShirts(quickSortRandomTShirtsBySize, 0, numberOfRandomTShirts - 1, 0, 0);
+        System.out.println("Bubble Sort Time: " + bubbleTimer.elapsedTime());
+
+        System.out.println("\nQuick Sorted Array By Size Ascending");
+        List<TShirt> quickSortRandomTShirtsBySize = new ArrayList<TShirt>(randomTShirts);
+        Timer quickTimer = new Timer();
+        s.quickSortTShirts(quickSortRandomTShirtsBySize, 0, numberOfRandomTShirts - 1, 0, 0);
+        quickTimer.stop();
 //        for (int i = 0; i < numberOfRandomTShirts; i++) {
 //            System.out.println(quickSortRandomTShirtsBySize.get(i));
 //        }
-//        
-//        System.out.println("\nQuick Sorted Array By Size Descending");
-//        List<TShirt> quickSortRandomTShirtsBySizeDesc = new ArrayList<TShirt>(randomTShirts);
-//        s.quickSortTShirts(quickSortRandomTShirtsBySizeDesc, 0, numberOfRandomTShirts - 1, 0, 1);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(quickSortRandomTShirtsBySizeDesc.get(i));
-//        }
-//        
-//        System.out.println("\nQuick Sorted Array By Color Ascending");
-//        List<TShirt> quickSortRandomTShirtsByColor = new ArrayList<TShirt>(randomTShirts);
-//        s.quickSortTShirts(quickSortRandomTShirtsByColor, 0, numberOfRandomTShirts - 1, 1, 0);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(quickSortRandomTShirtsByColor.get(i));
-//        }
-        
-//        System.out.println("\nQuick Sorted Array By Color Descending");
-//        List<TShirt> quickSortRandomTShirtsByColorDesc = new ArrayList<TShirt>(randomTShirts);
-//        s.quickSortTShirts(quickSortRandomTShirtsByColorDesc, 0, numberOfRandomTShirts - 1, 1, 1);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(quickSortRandomTShirtsByColorDesc.get(i));
-//        }
-        
-//        System.out.println("\nQuick Sorted Array By Fabric Ascending");
-//        List<TShirt> quickSortRandomTShirtsByFabric = new ArrayList<TShirt>(randomTShirts);
-//        s.quickSortTShirts(quickSortRandomTShirtsByFabric, 0, numberOfRandomTShirts - 1, 2, 0);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(quickSortRandomTShirtsByFabric.get(i));
-//        }
-        
-//        System.out.println("\nQuick Sorted Array By Fabric Descending");
-//        List<TShirt> quickSortRandomTShirtsByFabricDesc = new ArrayList<TShirt>(randomTShirts);
-//        s.quickSortTShirts(quickSortRandomTShirtsByFabricDesc, 0, numberOfRandomTShirts - 1, 2, 1);
-//        for (int i = 0; i < numberOfRandomTShirts; i++) {
-//            System.out.println(quickSortRandomTShirtsByFabricDesc.get(i));
-//        }
-//        
-        Vector<TShirt>[] buckets = new Vector[numberOfRandomTShirts]; 
-        
+        System.out.println("Quick Sort Time: " + quickTimer.elapsedTime());
     }
 }
